9dd8820d3137f5ae4283f854afe9f3a95f0694b7
Kam Brown <kbrown8055@outlook.com>
2019-08-18 17:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + ' plays ' + instuments[i])
+    array.push(musicians[i] + ' plays ' + instuments[i]);
   }
   return array
 }

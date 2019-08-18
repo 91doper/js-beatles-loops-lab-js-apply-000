97454cc2246925ed9b8a733dd80368f161b73d91
Kam Brown <kbrown8055@outlook.com>
2019-08-18 17:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
-  var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', "Ringo Starr"]
-  var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', "Ringo Starr"];
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
 
 theBeatlesPlay(musicians, instruments);
 
